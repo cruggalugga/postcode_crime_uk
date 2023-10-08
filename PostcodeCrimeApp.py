7e86b8fa376a9dd2b50845091b3727777a23df4e
cruggalugga <131597836+cruggalugga@users.noreply.github.com>
2023-10-08 16:27:57 +0100

    Add files via upload

---
+++ PostcodeCrimeApp.py
@@ -0,0 +1,141 @@
+import requests
+import json
+import streamlit as st
+import pandas as pd
+import math
+import re
+import pydeck as pdk
+
+# Use user postcode for API request to return latitude and longitude valuea
+def get_lat_long(postcode):
+    base_url = "https://api.postcodes.io/postcodes/"
+    url = base_url + postcode
+    try:
+        response = requests.get(url)
+        data =  json.loads(response.content)
+        if response.status_code == 200 and data['status'] == 200:
+            lat = data['result']['latitude']
+            lng = data['result']['longitude']
+            return lat, lng
+        else:
+            return None, None
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return None, None
+    
+# Function for parsing the data
+def parse_crime_event(cr):
+    cr_parsed = pd.Series({
+        'ID': cr['crime']['id'],
+        'Crime Category': cr['crime']['category'],
+        'Date': cr['crime']['month'],
+        'Category Code': cr['category']['code'],
+        'Cetegory Name': cr['category']['name'],
+        'Longitude': float(cr['crime']['location']['longitude']),
+        'Latitude': float(cr['crime']['location']['latitude']),
+        'Crime Location Type': cr['crime']['location_type'],
+        'Crime Location Sub-type': cr['crime']['location_subtype']
+    })
+    return cr_parsed
+
+# Function to get crime data for a given latitude and longitude
+def get_crime_data(lat, lng):
+    url = f'https://data.police.uk/api/outcomes-at-location?lat={lat}&lng={lng}'
+    r = requests.get(url)
+    crimes = json.loads(r.content)
+
+    parsed = []
+    for cr in crimes:
+        pc = parse_crime_event(cr)
+        parsed.append(pc)
+   
+    crime_parsed_df = pd.DataFrame(parsed)
+    return crime_parsed_df
+
+
+# Clean category text for the KPI box's
+def clean_category(category):
+    cleaned_category = re.sub(r'\W+', ' ', category).capitalize()
+    return cleaned_category
+
+# function to generate a grid with a KPI for each category count in the data
+def create_metric_boxes(category_counts, num_rows, num_columns, clean_category):
+    for i in range(num_rows):
+        columns = st.columns(num_columns)
+        for j in range(num_columns):
+            index = i * num_columns + j
+            if index < len(category_counts):
+                category = list(category_counts.keys())[index]
+                cleaned_category = clean_category(category)
+                count = category_counts[category]
+                columns[j].metric(label=f"{cleaned_category}", value=count)
+
+def total_crimes(crime_parsed_data):
+    crime_count = crime_parsed_data.shape[0]
+    return crime_count
+
+def draw_map(crime_parsed_data):
+    # Calculate the mean latitude and mean longitude
+    mean_latitude = crime_parsed_data['Latitude'].mean()
+    mean_longitude = crime_parsed_data['Longitude'].mean()
+
+    built_map = st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=mean_latitude,
+            longitude=mean_longitude,
+            zoom=13,
+            pitch=0,
+        ),
+        layers=[
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=crime_parsed_data,
+                get_position='[Longitude, Latitude]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=30,
+            ),
+        ],
+        tooltip=True
+    ))
+    return built_map
+
+
+# Main Section
+if __name__ == "__main__":
+    st.title(f":cop: Postcode Crime")
+    postcode = st.text_input("Enter a UK postcode:") # Text input for postcode
+
+    if postcode:
+        lat, lng = get_lat_long(postcode)
+        if lat and lng:
+            pass
+        else:
+            st.write("Invalid postcode or unable to retrieve location information.")
+
+        if lat and lng:
+            crime_parsed_data = get_crime_data(lat, lng)
+
+            # Aggregate count of rows for each crime category
+            category_counts = crime_parsed_data['Crime Category'].value_counts()
+
+            num_categories = len(category_counts)
+            num_columns = 4
+            num_rows = int(math.ceil(num_categories / num_columns))
+
+            st.title(f'Total Crimes :blue[{total_crimes(crime_parsed_data)}]')
+            create_metric_boxes(category_counts, num_rows, num_columns, clean_category)
+
+            st.subheader("Map")
+            draw_map(crime_parsed_data) # Function to draw the map
+
+            st.subheader("Table") 
+            st.write(crime_parsed_data) # Write out the df in a table
+
+
+           
+
+        
+            
+            
+        
